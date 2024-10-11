98401749766ac55e4a9c840e54b159c04aaafea6
Pushkar <pushkarcdn@gmail.com>
2024-10-11 11:30:49 +0545

    done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const socket = require('socket.io');
-const http = require('http');
-const { Chess } = require('chess.js');
-const path = require('path');
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,12 +11,68 @@ const io = socket(server);
 
 const chess = new Chess();
 
-let players = {}
-let currentPlayer = 'W';
+let players = {};
+let currentPlayer = "W";
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (uniquesocket) => {
+    console.log("A user connected");
+
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    uniquesocket.on("disconnect", () => {
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        } else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    });
+
+    uniquesocket.on("move", (move) => {
+
+        try {
+
+            if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+
+            if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+
+            if (result) {
+                currentPlayer = chess.turn()
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            } else {
+                console.log("Invalid move: ", move);
+                uniquesocket.emit("invalidMove: ", move);
+            } 
+
+        } catch (err) {
+
+            console.log(err);
+            uniquesocket.emit("invalidMove: ", move); 
+
+        }
+
+    });
+});
+
+server.listen(port, () => {
+    console.log("Server is running on port " + port);
 });
---
+++ public/js/chessGame.js
@@ -0,0 +1,162 @@
+const socket = io();
+
+const chess = new Chess();
+
+const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderboard = () => {
+
+    const board = chess.board();
+
+    boardElement.innerHTML = "";
+
+    board.forEach((row, rowindex) => {
+
+        row.forEach((square, squareindex) => {
+
+            const squareElement = document.createElement("div");
+
+            squareElement.classList.add(
+                "square",
+                (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+            );
+
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            if (square) {
+
+                const pieceElement = document.createElement("div");
+
+                pieceElement.classList.add(
+                    "piece",
+                    square.color === "w" ? "white" : "black",
+                );
+
+                pieceElement.innerText = getPieceUnicode(square);
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart", () => {
+
+                    if (pieceElement.draggable) {
+                        draggedPiece = pieceElement;
+                        sourceSquare = {
+                            row: rowindex,
+                            col: squareindex
+                        }
+                        e.dataTransfer.setData("text/plain", "");
+                    }
+                });
+
+                pieceElement.addEventListener("dragend", (e) => {
+                    draggedPiece = null;
+                    sourceSquare = null;
+                });
+
+                squareElement.appendChild(pieceElement);
+
+            }
+
+            squareElement.addEventListener("dragover", (e) => {
+                e.preventDefault();
+            });
+
+            squareElement.addEventListener("drop", (e) => {
+
+                e.preventDefault();
+
+                if (!draggedPiece) return;
+
+                const targetSquare = {
+                    row: parseInt(squareElement.dataset.row),
+                    col: parseInt(squareElement.dataset.col)
+                }
+
+                const move = {
+                    from: {
+                        row: sourceSquare.row,
+                        col: sourceSquare.col
+                    },
+                    to: {
+                        row: targetSquare.row,
+                        col: targetSquare.col
+                    }
+                }
+
+                handleMove(sourceSquare, targetSquare);
+
+
+            });
+
+            boardElement.appendChild(squareElement);
+
+        });
+
+    });
+
+    if (playerRole === 'b') {
+        boardElement.classList.add("flipped");
+    } else {
+        boardElement.classList.remove("flipped");
+    }
+
+}
+
+const handleMove = (source, target) => {
+
+    const move = {
+        from: `${String.fromCharCode(97 + source.col)}${8 - source.row}`,
+        to: `${String.fromCharCode(97 + target.col)}${8 - target.row}`,
+        promotion: "q"
+    }
+
+    socket.emit("move", move);
+
+}
+
+const getPieceUnicode = (piece) => {
+    const unicodePieces = {
+        p: "\u2659",
+        r: "\u2656",
+        n: "\u2658",
+        b: "\u2657",
+        q: "\u2655",
+        k: "\u2654",
+        P: "\u265F",
+        R: "\u265C",
+        N: "\u265E",
+        B: "\u265D",
+        Q: "\u265B",
+        K: "\u265A"
+    };
+
+    return unicodePieces[piece.type] || "";
+
+}
+
+socket.on("playerRole", (role) => {
+    playerRole = role;
+    renderboard();
+});
+
+socket.on("spectatorRole", () => {
+    playerRole = null;
+    renderboard();
+})
+
+socket.on("boardState", (fen) => {
+    chess.load(fen);
+    renderboard();
+});
+
+socket.on("move", (move) => {
+    chess.move(move);
+    renderboard();
+});
+
+
+renderboard();
